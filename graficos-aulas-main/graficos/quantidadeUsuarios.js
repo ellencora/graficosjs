ee499714cd714180462ff82318142c2c4e2a89c4
ellencora <131362080+ellencora@users.noreply.github.com>
2024-11-18 09:11:32 -0300

    Add files via upload

---
+++ graficos-aulas-main/graficos/quantidadeUsuarios.js
@@ -0,0 +1,27 @@
+import { getCSS } from "./common.js";
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/educacao/educacao-dados-globais.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker:{
+                color:'purple'
+            }
+        }
+    ]
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data)
+}
+
+quantidadeUsuariosPorRede()
